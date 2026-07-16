105c3d04ab984e600d548414f54f77e94337156a
devsharma6 <devsharma60022@gamil.com>
2026-07-17 01:20:09 +0530

    Day 5: Integrated direction module into main pipeline

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from AI.object_detection.camera import Camera
 from AI.object_detection.detector import Detector
+from AI.object_detection.direction import get_direction
 
 import cv2
 
@@ -19,6 +20,29 @@ while True:
     results = detector.detect(frame)
 
     annotated_frame = results[0].plot()
+
+    for box in results[0].boxes:
+
+        cls = int(box.cls[0])
+        object_name = detector.model.names[cls]
+
+        x1, y1, x2, y2 = box.xyxy[0]
+
+        center_x = (x1 + x2) / 2
+
+        direction = get_direction(center_x, frame.shape[1])
+
+        print(f"{object_name} --> {direction}")
+
+        cv2.putText(
+            annotated_frame,
+            direction,
+            (int(x1), int(y1) - 15),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.7,
+            (0, 255, 0),
+            2
+        )
 
     cv2.imshow("VisionMate AI", annotated_frame)
 
